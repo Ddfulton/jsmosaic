a8fc5e9f233a63ff502773c983d43485ad7e8605
Derek <ddfulton@live.unc.edu>
2017-08-20 21:29:44 +0000

    Commit from DO

--- index.js
+++ index.js
@@ -2,13 +2,19 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+let pug = require('pug');
 
 // Define the port to run on
 app.set('port', 8080);
 
+// Define static directories
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Listen for requests
+app.get('/mosaics', function (req, res) {
+  res.sendFile(__dirname + '/public/mosaics.html');
+})
+
+// Listen for requests on '/'
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log('Magic happens on port ' + port);
